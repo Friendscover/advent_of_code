fe47537870758b17dfd0790f382af878ec83c354
Tobias Klöpper <tobiaskloepper1@web.de>
2022-12-08 15:17:30 +0100

    finished day 04

--- 2022/day_04/day_04.rb
+++ 2022/day_04/day_04.rb
@@ -3,6 +3,7 @@ def main
 
   # initialize counter
   double_pairs = 0
+  overlaps = 0
 
   input.each do |element|
     range_array = element.map { |i| i.split('-') }
@@ -11,6 +12,12 @@ def main
     range_a = (range_array[0]..range_array[1]).to_a
     range_b = (range_array[2]..range_array[3]).to_a
 
+    # part 2
+    intersection = range_a.intersection(range_b)
+
+    intersection.empty? ? next : overlaps += 1
+
+    # part 1
     if range_a.length > range_b.length
       check_full_containmnent(range_a, range_b) ? double_pairs += 1 : next
     else
@@ -19,12 +26,13 @@ def main
   end
 
   p double_pairs
+  p overlaps
 end
 
 def check_full_containmnent(long_array, short_array)
   short_array.each do |item|
     next if long_array.include?(item)
-    
+
     return false
   end
 
