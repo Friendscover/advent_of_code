1847d0552e7e72ba55290b37f9b86d1e48e2a4b0
Tobias Klöpper <tobiaskloepper1@web.de>
2022-12-08 15:05:38 +0100

    added the solution for day4 part1

---
+++ 2022/day_04/day_04.rb
@@ -0,0 +1,34 @@
+def main
+  input = File.readlines('input.txt').map { |i| i.chomp.split(',') }
+
+  # initialize counter
+  double_pairs = 0
+
+  input.each do |element|
+    range_array = element.map { |i| i.split('-') }
+    range_array = range_array.flatten.map(&:to_i)
+
+    range_a = (range_array[0]..range_array[1]).to_a
+    range_b = (range_array[2]..range_array[3]).to_a
+
+    if range_a.length > range_b.length
+      check_full_containmnent(range_a, range_b) ? double_pairs += 1 : next
+    else
+      check_full_containmnent(range_b, range_a) ? double_pairs += 1 : next
+    end
+  end
+
+  p double_pairs
+end
+
+def check_full_containmnent(long_array, short_array)
+  short_array.each do |item|
+    next if long_array.include?(item)
+    
+    return false
+  end
+
+  true
+end
+
+main
