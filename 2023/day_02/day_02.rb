491404807fa747e14ce5236b42738f60c62d546f
Tobias Klöpper <tobiaskloepper1@web.de>
2023-12-22 13:25:56 +0100

    finished day 2 part 1

---
+++ 2023/day_02/day_02.rb
@@ -0,0 +1,50 @@
+def main
+  input = File.read('input.txt').split("\n")
+
+  valid_games_sum = 0
+
+  input.each do |item|
+    # split
+    game = item.split(':')[0]
+    game_id = game.split(' ')[1].to_i
+
+    # split to get game data
+    game_data = item.split(':')[1]
+
+    game_data = game_data.split(';')
+
+    # check data if game is valid
+    next unless valid_game?(game_data)
+    valid_games_sum += game_id
+  end
+
+  p valid_games_sum
+end
+
+def valid_game?(game_data)
+  # max_values
+  max_red = 12
+  max_green = 13
+  max_blue = 14
+
+  round = game_data.map { |g| g.split(',') }
+
+  round.each do |draw|
+    draw.each do |cubes|
+      cubes = cubes.split(' ')
+
+      # check if values are greater than max
+      if cubes[1] == 'red'
+        return false if cubes[0].to_i > max_red
+      elsif cubes[1] == 'green'
+        return false if cubes[0].to_i > max_green
+      elsif cubes[0].to_i > max_blue
+        return false
+      end
+    end
+  end
+
+  true
+end
+
+main
