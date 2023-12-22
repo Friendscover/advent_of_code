19faef19fe769e601145f240836e2c30021a8b62
Tobias Klöpper <tobiaskloepper1@web.de>
2023-12-22 11:58:42 +0100

    added day 1 part 2

--- 2023/day_01/day_01.rb
+++ 2023/day_01/day_01.rb
@@ -3,14 +3,26 @@ def main
   input = File.readlines('input.txt').map(&:chomp)
 
   sum = 0
+  number_patterns = %w[one two three four five six seven eight nine]
 
   input.each do |items|
-    # replace all non number chars with a  star and delete them from the array
-    items.gsub!(/[a-zA-Z]/, '*')
-    items = items.split(//).delete_if { |i| i == '*' }
+    # part2 replaces written numbers with numbers in between
+    items.gsub!(number_patterns[0], 'on1e')
+    items.gsub!(number_patterns[1], 'tw2o')
+    items.gsub!(number_patterns[2], 'thr3ee')
+    items.gsub!(number_patterns[3], 'fo4ur')
+    items.gsub!(number_patterns[4], 'fi5ve')
+    items.gsub!(number_patterns[5], 'si6x')
+    items.gsub!(number_patterns[6], 'se7ven')
+    items.gsub!(number_patterns[7], 'eigh8te')
+    items.gsub!(number_patterns[8], 'ni9ne')
 
-    # concat the strings from the array and convert them to ints 
-    number = items.first + items.last
+    # replace all non number chars with a  star and delete them from the array
+    coordinates = items.gsub(/[a-zA-Z]/, '*')
+    coordinates = coordinates.split(//).delete_if { |i| i == '*' }
+
+    # concat the strings from the array and convert them to ints
+    number = coordinates.first + coordinates.last
     sum += number.to_i
   end
 
