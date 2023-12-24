fbd5139e306a4e0a9831b2ad0b2955c874375478
Tobias Klöpper <tobiaskloepper1@web.de>
2023-12-24 15:26:27 +0100

    added the solution for day 2 part 2

--- 2023/day_02/day_02.rb
+++ 2023/day_02/day_02.rb
@@ -2,6 +2,7 @@ def main
   input = File.read('input.txt').split("\n")
 
   valid_games_sum = 0
+  fewest_cubes_sum = 0
 
   input.each do |item|
     # split
@@ -14,8 +15,11 @@ def main
     game_data = game_data.split(';')
 
     # check data if game is valid
-    next unless valid_game?(game_data)
-    valid_games_sum += game_id
+    valid_games_sum += game_id if valid_game?(game_data)
+
+    # part 2
+    fewest_cubes_sum += calculate_fewest_cubes(game_data)
+    p fewest_cubes_sum
   end
 
   p valid_games_sum
@@ -45,6 +49,31 @@ def valid_game?(game_data)
   end
 
   true
+end
+
+def calculate_fewest_cubes(game_data)
+  fewest_red = 0
+  fewest_green = 0
+  fewest_blue = 0
+
+  round = game_data.map { |g| g.split(',') }
+
+  round.each do |draw|
+    draw.each do |cubes|
+      cubes = cubes.split(' ')
+
+      # check if values are greater than fewest
+      if cubes[1] == 'red'
+        fewest_red = cubes[0].to_i if cubes[0].to_i > fewest_red
+      elsif cubes[1] == 'green'
+        fewest_green = cubes[0].to_i if cubes[0].to_i > fewest_green
+      elsif cubes[0].to_i > fewest_blue
+        fewest_blue = cubes[0].to_i
+      end
+    end
+  end
+
+  (fewest_red * fewest_green * fewest_blue)
 end
 
 main
